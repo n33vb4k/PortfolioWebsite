8d6cc1378074175ac75ee6d6ce547f9d13b15fa5
n33vb4k <bakshi.neev@gmail.com>
2025-01-21 00:03:00 +0000

    shadow test

--- portfoliosite/src/App.tsx
+++ portfoliosite/src/App.tsx
@@ -3,7 +3,7 @@ import SideBar from "./components/SideBar"
 function App() {
   return (
     <>
-      <p className="text-center text-4xl text- text-indigo-500 font-bold">
+      <p className="text-center text-4xl text- text-indigo-500 font-bold shadow-lg">
         Hello World!
       </p>
       <div className="flex">
